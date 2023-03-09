546fad7796ea2ed409259b98282aef748eef3fd7
Carles Galan <galan.carles@gmail.com>
2023-03-09 17:39:11 +0100

    Change cors policy to able new origin

--- jest.config.cjs
+++ jest.config.cjs
@@ -7,13 +7,7 @@ module.exports = {
   collectCoverageFrom: [
     "src/**/*.ts",
     "!src/index.ts",
-    "!src/react-app-env.d.ts",
-    "!src/setupTests.ts",
-    "!src/server/index.ts",
-    "!src/server/App.ts",
-    "!src/server/cors.ts",
     "!src/server/startServer.ts",
-    "!src/loadEnvironment.ts",
     "!src/database/connectDataBase.ts",
   ],
 };
