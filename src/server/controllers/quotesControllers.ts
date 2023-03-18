38ad1c3919ddd99b99ffc35877447f296ed545c1
Carles Galan <galan.carles@gmail.com>
2023-03-18 14:45:14 +0100

    Create deleteQuote controller

--- src/server/controllers/quotesControllers.ts
+++ src/server/controllers/quotesControllers.ts
@@ -3,6 +3,7 @@ import createDebug from "debug";
 import { Quote } from "../../database/models/Quote.js";
 import CustomError from "../../CustomError/CustomError.js";
 import statusCodes from "../utils/statusCodes.js";
+import mongoose from "mongoose";
 
 const {
   clientError: { notFound },
@@ -29,5 +30,29 @@ export const getQuotes = async (
     res.status(200).json({ quotes });
   } catch (error) {
     next(error);
+  }
+};
+
+export const deleteQuote = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const id = req.params?.id;
+
+  try {
+    if (!mongoose.isValidObjectId(id)) {
+      throw new CustomError("Invalid object id!", 400, "Invalid data!");
+    }
+
+    const quoteToDelete = await Quote.findByIdAndDelete(id).exec();
+
+    if (!quoteToDelete) {
+      throw new CustomError("Mongoose method failed!", 404, "Couldn't delete!");
+    }
+
+    res.status(200).json({ message: `${quoteToDelete.author} deleted!` });
+  } catch (error) {
+    next(new CustomError((error as Error).message, 400, "Couldn't delete!"));
   }
 };
